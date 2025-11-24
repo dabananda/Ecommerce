eb67b14566bddc9790fecf25121974558c53cc60
dabananda <dabananda.dev@gmail.com>
2025-11-24 18:28:31 +0600

    Add EF Core with Product entity and initial migration

---
+++ ECommerce.Api/Data/ECommerceDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ECommerce.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Api.Data
+{
+    public class ECommerceDbContext : DbContext
+    {
+        public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- ECommerce.Api/Program.cs
+++ ECommerce.Api/Program.cs
@@ -1,14 +1,16 @@
+using ECommerce.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ECommerceDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ECommerceDbConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
